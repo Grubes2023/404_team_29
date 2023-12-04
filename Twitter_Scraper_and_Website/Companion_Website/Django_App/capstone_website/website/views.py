9d51cb11cbc2e99b9ec47f124e4cfdc3f6f25efa
Grubes2023 <58446725+Grubes2023@users.noreply.github.com>
2023-12-04 17:23:25 -0600

    Delete Twitter_Scraper_and_Website directory

--- Twitter_Scraper_and_Website/Companion_Website/Django_App/capstone_website/website/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-
-def index(request): #creating the url connection for the home page
-    return render(request, 'index.html', {})
-def about(request):#creating the url connection for the help page
-    return render(request, 'about.html', {})
